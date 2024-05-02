c9cc23e0b9c7eb5a635f6037b1f915060b160f17
Youssef Yasser <142177636+Joao1160@users.noreply.github.com>
2024-05-03 00:23:36 +0300

    Add files via upload

---
+++ Models/Product.php
@@ -0,0 +1,18 @@
+<?php
+require_once("../../Controllers/DBcontroller.php");
+class Product
+{
+    private $id;
+    private $name;
+    private $email;
+    private $price;
+    private $category;
+    private $image;
+    private $description;
+    private $rate;
+    private $brand;
+    private $sellerId;
+
+}
+
+
