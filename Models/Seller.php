16fbbb2581ae5dddaaaae6d0312f448318fcb064
Momen <123340262+momenelbarrawy@users.noreply.github.com>
2024-05-04 18:16:34 +0300

    Update Seller.php

--- Models/Seller.php
+++ Models/Seller.php
@@ -1,54 +1,39 @@
 <?php
-require_once("../Controllers/DBcontroller.php");
-require_once("../Controllers/EditProduct.php");
+require_once '../../online_shipping/Controllers/DBcontroller.php';
+require_once '../../online_shipping/Controllers/Editor.php';
 $dbController = new DBController();
 $dbController->openConnection();
-class Seller extends Editor
+$edit = new Editor();
+class Seller extends User
 {
-    private $dbController;
-    private $name;
-    private $email;
 
-    private $password;
-    private $phone;
-
-    public function register($role,$name , $email , $password , $phone)
+    public function register($role)
     {
-        $this->name = $name;
-        $this->email = $email;
-        $this->password = $password;
-        $this->phone = $phone;
-
-        $db=new DBController;
-        if($db->openConnection()){
-            $query="insert into users values ('','$name','$email','$password','$phone','$role','active')";
-            $result=$db->insert($query);
-            if($result!=false)
-            {
+        $db = new DBController;
+        if ($db->openConnection()) {
+            $query = "insert into users values ('','$this->name','$this->email','$this->password','$this->phone','$role','active')";
+            $result = $db->insert($query);
+            if ($result != false) {
                 session_start();
-                $_SESSION["userid"]=$result;
-                $_SESSION["username"]=$this->name;
-                $_SESSION["userrole"]=$role;
+                $_SESSION["userid"] = $result;
+                $_SESSION["username"] = $this->name;
+                $_SESSION["userrole"] = $role;
                 $db->closeConnection();
                 return true;
-            }
-            else
-            {
-                $_SESSION["errMsg"]="Somthing went wrong... try again later";
+            } else {
+                $_SESSION["errMsg"] = "Somthing went wrong... try again later";
                 $db->closeConnection();
                 return false;
             }
-        }
-        else
-        {
+        } else {
             echo "Error in Database Connection";
             return false;
         }
     }
-    
+
     public function addProduct($productName, $price, $category, $brand, $image, $description, $sellerid, $roll)
     {
-        $db=new DBController;
+        $db = new DBController;
         $db->openConnection();
         if ($db->getConnection() === null) {
             echo "Database connection is not established.";
@@ -68,11 +53,11 @@ class Seller extends Editor
         }
 
         $stmt->close();
-    }   
+    }
 
     public function removeProduct($productId)
     {
-        $db=new DBController;
+        $db = new DBController;
         $db->openConnection();
         if ($db->getConnection() === null) {
             echo "Database connection is not established.";
@@ -93,5 +78,171 @@ class Seller extends Editor
 
         $stmt->close();
     }
-    public function replayComments(){}
+    public function updateProduct($feild, $productId, $newValue)
+    {
+        $e = new Editor;
+        switch ($feild) {
+            case "pname":
+                $e->editProductName($productId, $newValue);
+                break;
+            case "price":
+                $e->editProductPrice($productId, $newValue);
+                break;
+            case "categroy":
+                $e->editProductCategory($productId, $newValue);
+                break;
+            case "description":
+                $e->editProductDescription($productId, $newValue);
+                break;
+            case "image":
+                $e->editProductImage($productId, $newValue);
+                break;
+            case "brand":
+                $e->editProductBrand($productId, $newValue);
+                break;
+            case "rate":
+                $e->editProductRate($productId, $newValue);
+                break;
+            case "roll":
+                $e->editProductRoll($productId, $newValue);
+                break;
+        }
+    }
+    public function updateProfile($feild, $productId, $newValue)
+    {
+        $e = new Editor;
+        switch ($feild) {
+            case "usernamename":
+                $e->editProfileName($productId, $newValue);
+                break;
+            case "email":
+                $e->editProfileEmail($productId, $newValue);
+                break;
+            case "password":
+                $e->editProfilePassword($productId, $newValue);
+                break;
+            case "phone":
+                $e->editProfilePhone($productId, $newValue);
+                break;
+            case "role":
+                $e->editProfileRole($productId, $newValue);
+                break;
+        }
+    }
+    public function replayComments()
+    {
+    }
+}
+
+
+class User
+{
+    protected $id;
+    protected $name;
+    protected $email;
+    protected $password;
+    protected $phone;
+    protected $role;
+
+    public function setname($name)
+    {
+        $this->name = $name;
+    }
+    public function setemail($email)
+    {
+        $this->email = $email;
+    }
+    public function setpassword($pass)
+    {
+        $this->password = $pass;
+    }
+    public function setphone($phone)
+    {
+        $this->phone = $phone;
+    }
+    public function getname()
+    {
+        return $this->name;
+    }
+    public function getemail()
+    {
+        return $this->email;
+    }
+    public function getpassword()
+    {
+        return $this->password;
+    }
+    public function getphone()
+    {
+        return $this->phone;
+    }
+    public function setid($id)
+    {
+        $this->phone = $id;
+    }
+    public function getid()
+    {
+        return $this->id;
+    }
+    public function setrole($role)
+    {
+        $this->phone = $role;
+    }
+    public function getrole()
+    {
+        return $this->role;
+    }
+
+
+    public function login()
+    {
+        $db = new DBcontroller;
+        if ($db->openConnection()) {
+            $query = "select userid ,username ,password , role from users where username = '$this->name'";
+            $result = $db->select($query);
+            if ($result != false) {
+                if ($result[0]["password"] == $this->password) {
+                    $query = "update users set loginstatus = 'active' where username = '$this->name'";
+                    $db->insert($query);
+                    session_start();
+                    $_SESSION["userid"] = $result['userid'];
+                    $_SESSION["username"] = $result['username'];
+                    $_SESSION["userrole"] = $result['role'];
+                    $db->closeConnection();
+                    return true;
+                } else {
+                    echo "<div class=\"alert alert-danger alert-dismissible py-3\">
+                <button type=\"button\" class=\"btn-close\" data-bs-dismiss=\"alert\"></button>
+                Error :password is worng </div>";
+                    $db->closeConnection();
+                    return false;
+                }
+            } else {
+                echo "<div class=\"alert alert-danger alert-dismissible py-3\">
+        <button type=\"button\" class=\"btn-close\" data-bs-dismiss=\"alert\"></button>
+        Error :user not found </div>";
+                $db->closeConnection();
+                return false;
+            }
+        } else {
+            echo "<div class=\"alert alert-danger alert-dismissible py-3\">
+            <button type=\"button\" class=\"btn-close\" data-bs-dismiss=\"alert\"></button>
+            Error : Database Connection</div>";
+            return false;
+        }
+    }
+
+    public function logout()
+    {
+        $db = new DBcontroller;
+        if ($db->openConnection()) {
+            $query = "update users set loginstatus = 'disactive' where username = '$this->name'";
+            $db->insert($query);
+        } else {
+            echo "<div class=\"alert alert-danger alert-dismissible py-3\">
+            <button type=\"button\" class=\"btn-close\" data-bs-dismiss=\"alert\"></button>
+            Error : Database Connection</div>";
+            return false;
+        }
+    }
 }
